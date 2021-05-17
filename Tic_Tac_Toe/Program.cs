ff9184bb9dbfc0436bd6006cdf931df21d9c12ab
jagtapnn0509 <jagtapnn05@gmail.com>
2021-05-17 12:56:29 +0530

    Add : function to get empty board

--- Tic_Tac_Toe/Program.cs
+++ Tic_Tac_Toe/Program.cs
@@ -4,9 +4,19 @@ namespace Tic_Tac_Toe
 {
     class Program
     {
-        static void Main(string[] args)
+        static void EmptyBorad()
+        {
+            int boardSize = 10;
+            char[] board = new char[boardSize];
+            for (int i = 1; i < boardSize; i++)
+            {
+                board[i] = ' ';
+            }
+        }
+            static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Tic Tac Toe Game");
+            EmptyBorad();
         }
     }
 }
