65d1a8455eeff2b6679ba9e75a14b717592ee994
jagtapnn0509 <jagtapnn05@gmail.com>
2021-05-18 08:46:12 +0530

    Add : function toCreate the Game board

--- Tic_Tac_Toe/Program.cs
+++ Tic_Tac_Toe/Program.cs
@@ -4,14 +4,34 @@ namespace Tic_Tac_Toe
 {
     class Program
     {
-        static void EmptyBorad()
+        static void TicTacToeBorad()
         {
+
             int boardSize = 10;
             char[] board = new char[boardSize];
             for (int i = 1; i < boardSize; i++)
             {
                 board[i] = ' ';
             }
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", board[1], board[2], board[3]);
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", board[4], board[5], board[6]);
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", board[7], board[8], board[9]);
+
+            Console.WriteLine("     |     |      ");
+
         }
 
         static void ChooseLetter()
@@ -41,8 +61,8 @@ namespace Tic_Tac_Toe
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Tic Tac Toe Game");
-            EmptyBorad();
             ChooseLetter();
+            TicTacToeBorad();
         }
     }
 }
